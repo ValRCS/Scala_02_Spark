17b4c9ce499f343316d52256c5cc7d48b9131cfd
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-11-30 20:20:08 +0200

    Splitting Dataframe and sampling dataframe

--- src/main/scala/ReadingCSV.scala
+++ src/main/scala/ReadingCSV.scala
@@ -22,6 +22,7 @@ object ReadingCSV extends App {
     .withColumn("CustomerID", col("CustomerID").cast("int"))
 //    .withColumn("InvoiceDate", col("InvoiceDate").cast("date"))
     .withColumn("InvoiceDate", col("InvoiceDate").cast("timestamp"))
+    .limit(3000)
   df2.printSchema()
   df2.summary().show()
   val negQuantity = df2.filter(col("Quantity") < 0)
@@ -36,5 +37,29 @@ object ReadingCSV extends App {
   df2.select("Country").distinct().show()
   println(df2.select("Country").distinct().count()) //should be 7
   //TODO select single entries for each country meaning full rows
+
+  //Random sampling
+  val seed = 42
+  val withReplacement = false //when it is false it will not pick the same row more than 1 time
+  val fraction = 0.5 //so 20%
+  val dSample = df2.sample(withReplacement, fraction, seed)
+  println("Dataframe size", df2.count(), df2.distinct().count())
+
+  println(dSample.count())
+  println(dSample.distinct().count())
+  //Turns out there are duplicate rows already in the original dataframe
+
+  val replSample = df2.sample(true, fraction, seed)
+  println(replSample.count())
+  println(replSample.distinct().count())
+
+  //Splitting into two dataframes/datasets
+  // in Scala
+  val dataFrames = df2.randomSplit(Array(0.25, 0.75), seed)
+  println(dataFrames(0).count(), dataFrames(1).count()) // False
+
+  val propFrames= df2.randomSplit(Array(5, 4, 3), seed)
+  propFrames.foreach(df => println(df.count())) // False
+
 
 }
