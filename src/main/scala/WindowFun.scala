d30f14ec86ea6b4ba70e471ac39faaf75db68ce0
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-12-07 20:57:13 +0200

    Simple GroupBY

--- src/main/scala/WindowFun.scala
+++ src/main/scala/WindowFun.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, to_date}
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions.{col, dense_rank, desc, max, mean, min, rank, to_date}
 
 object WindowFun extends App {
   val session = SparkSession.builder().appName("test").master("local").getOrCreate()
@@ -16,5 +17,43 @@ object WindowFun extends App {
     .withColumn("date", to_date(col("InvoiceDate"), "MM/d/yyyy H:mm"))
   df.printSchema()
   df.show(5, false)
+
+  val windowSpec = Window
+    .partitionBy("CustomerId", "date")
+    .orderBy(col("Quantity").desc)
+    .rowsBetween(Window.unboundedPreceding, Window.currentRow)
+
+
+  val maxPurchaseQuantity = max(col("Quantity")).over(windowSpec)
+  val meanPurchaseQuantity = mean(col("Quantity")).over(windowSpec)
+  val minPurchaseQuantity = min(col("Quantity")).over(windowSpec)
+  //TODO explore why min is giving the current
+  val purchaseDenseRank = dense_rank().over(windowSpec)
+  val purchaseRank = rank().over(windowSpec)
+
+  df.where("CustomerId IS NOT NULL").orderBy("CustomerId")
+    .select(
+      col("CustomerId"),
+      col("InvoiceNo"),
+      col("Description"),
+      col("date"),
+      col("Quantity"),
+      purchaseRank.alias("quantityRank"),
+      purchaseDenseRank.alias("quantityDenseRank"),
+      minPurchaseQuantity.alias("minPurchaseQuantity"),
+      meanPurchaseQuantity.alias("meanPurchaseQuantity"),
+      maxPurchaseQuantity.alias("maxPurchaseQuantity"))
+    .show(50, false)
+
+  // in Scala
+  val dfNoNull = df.drop()
+  dfNoNull.createOrReplaceTempView("dfNoNull")
+  dfNoNull
+    .groupBy("customerId", "stockCode")
+    .agg("Quantity"->"sum"
+    )
+    .orderBy(desc("customerId"), desc("stockCode"))
+    .show(5,false)
+
 
 }
