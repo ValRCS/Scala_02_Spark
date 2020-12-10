8a55bf6bf1a8344cf70a10efc897d538459232ff
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-12-10 19:39:02 +0200

    reading tables

--- src/main/scala/SQLConn.scala
+++ src/main/scala/SQLConn.scala
@@ -1,3 +1,6 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, desc, expr}
+
 object SQLConn extends App {
   println("Testing SQL connection")
   val driver = "org.sqlite.JDBC"
@@ -5,8 +8,31 @@ object SQLConn extends App {
   val url = s"jdbc:sqlite:/${path}"
   val tableName = "genres"
 
-  import java.sql.DriverManager
-  val connection = DriverManager.getConnection(url)
-  connection.isClosed()
-  connection.close()
+  //good for troubleshooting connections
+//  import java.sql.DriverManager
+//  val connection = DriverManager.getConnection(url)
+//  connection.isClosed()
+//  connection.close()
+
+  val session = SparkSession.builder().appName("test").master("local").getOrCreate()
+
+
+  // in Scala
+  val df = session.read.format("jdbc").option("url", url)
+    .option("dbtable", tableName).option("driver", driver).load()
+
+  df.printSchema()
+  df.show(5, false)
+
+  val artists =  session.read.format("jdbc").option("url", url)
+    .option("dbtable", "artists").option("driver", driver).load()
+  artists.printSchema()
+  artists.sample(0.01).show(25,false)
+  artists.sort(desc("Name")).show(25)
+  artists.sort(expr("Name desc")).show(15, false)
+
+
+  val tracks = session.read.format("jdbc").option("url", url)
+    .option("dbtable", "tracks").option("driver", driver).load()
+  tracks.printSchema()
 }
