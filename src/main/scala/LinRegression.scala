85b20fa74ecb53138d50bb027ec6ca50398797d5
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-12-22 21:59:08 +0200

    More regression

--- src/main/scala/LinRegression.scala
+++ src/main/scala/LinRegression.scala
@@ -1,6 +1,9 @@
-import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.linalg.SQLDataTypes.VectorType
+import org.apache.spark.ml.linalg.{Vectors, DenseVector}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, udf}
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{StructField, StructType}
 
 object LinRegression extends App {
   val spark = SparkSession.builder().appName("test").master("local").getOrCreate()
@@ -11,10 +14,11 @@ object LinRegression extends App {
   df.selectExpr("label").distinct().show()
 
   import org.apache.spark.ml.regression.LinearRegression
-  val lr = new LinearRegression()
-    .setMaxIter(10)
-    .setRegParam(0.3)
-    .setElasticNetParam(0.8)
+  val lr = new LinearRegression() //defaults are fine
+//    .setMaxIter(10)
+//    .setRegParam(0.3)
+//    .setElasticNetParam(0.8) //one of these has a bug/feature that makes all values same
+
   println(lr.explainParams())
   val lrModel = lr.fit(df)
 
@@ -47,7 +51,70 @@ object LinRegression extends App {
   gdf.printSchema()
   gdf.show(false)
 
-//  val gradeModel = lr.fit(gdf)
-//  val gradeSummary = gradeModel.summary
-//  gradeSummary.residuals.show()
+
+  val gradeModel = lr.fit(gdf)
+  val gradeSummary = gradeModel.summary
+  gradeSummary.residuals.show()
+
+  gdf.show(5, false)
+  gradeModel.transform(gdf).show(5,false)
+
+  gradeModel.transform(gdf.selectExpr("features")).show(15, false)
+
+  val inputs = Seq(Row(Vectors.dense(1200)),
+    Row(Vectors.dense(1500)),
+    Row(Vectors.dense(1800)),
+    Row(Vectors.dense(2100)),
+  )
+
+  val someSchema = List(
+    StructField("features", VectorType, true)
+  )
+  val inDF = spark.createDataFrame(
+    spark.sparkContext.parallelize(inputs),
+    StructType(someSchema)
+  )
+
+  inDF.printSchema()
+  inDF.show(false)
+
+  gradeModel.transform(inDF).show(false)
+
+  //TODO get the coefficients
+  // Here are the coefficient and intercept
+//  val weights: org.apache.spark.mllib.linalg.Vector = gradeModel
+  val weights = gradeModel.coefficients
+  val intercept = gradeModel.intercept
+  val weightsData: Array[Double] = weights.asInstanceOf[DenseVector].values
+
+  println(s"Intercept is $intercept")
+  weightsData.foreach(println)
+//  weightsData.foreach(coef: Double => println(s"Coef is $coef"))
+
+  import org.apache.spark.ml.regression.GeneralizedLinearRegression
+  val glr = new GeneralizedLinearRegression()
+    .setFamily("gaussian")
+    .setLink("identity")
+    .setMaxIter(10)
+    .setRegParam(0.3)
+    .setLinkPredictionCol("linkOut")
+  println(glr.explainParams())
+  val glrModel = glr.fit(gdf)
+  println(s"Intercept is ${glrModel.intercept}")
+  glrModel.coefficients.asInstanceOf[DenseVector].values.foreach(println)
+
+
+  //The residuals
+  //The difference between the label and the predicted value.
+  lrModel.summary.residuals.show(5)
+  glrModel.summary.residuals.show(5)
+  println("Default LR Model")
+  println("rootMeanSquaredError", lrModel.summary.rootMeanSquaredError)
+  println("r2", lrModel.summary.r2)
+  println("Generalized LR Model with some hyperparameters set")
+  println("Deviance", glrModel.summary.deviance)
+//  println(glrModel.summary.r2)
+
+  //R squared
+  //The coefficient of determination; a measure of fit. so bas
 }
