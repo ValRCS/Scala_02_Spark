1b1fb03ecaa15b3d22edd9cbf095087cf3f230c8
Valdis Saulespurens <valdis.s.coding@gmail.com>
2020-12-28 21:29:31 +0200

    Topic Modeling

--- src/main/scala/ClusteringData.scala
+++ src/main/scala/ClusteringData.scala
@@ -15,7 +15,7 @@ object ClusteringData extends App {
     .option("header", "true")
     .option("inferSchema", "true")
     .load("./src/resources/retail-data/by-day/*.csv")
-    .limit(50)
+    .limit(200)
     .coalesce(1)
     .where("Description IS NOT NULL"))
   sales.cache() //for quicker analysis
@@ -66,9 +66,60 @@ object ClusteringData extends App {
   import org.apache.spark.ml.clustering.BisectingKMeans
   val bkm = new BisectingKMeans().setK(5).setMaxIter(5)
 
+  val bkmModel = bkm.fit(sales)
+  bkmModel.transform(sales).show(20,false)
+
   //TODO again model and make prediction on our sales!
   import org.apache.spark.ml.clustering.GaussianMixture
   val gmm = new GaussianMixture().setK(5)
+  val gmmModel = gmm.fit(sales)
+  gmmModel.transform(sales).show(20,false)
+
+  //Latent Dirichlet Allocation (LDA) is a hierarchical clustering model typically used to perform
+  //topic modelling on text documents. LDA tries to extract high-level topics from a series of
+  //documents and keywords associated with those topics. It then interprets each document as having
+  //a variable number of contributions from multiple input topics. There are two implementations
+  //that you can use: online LDA and expectation maximization. In general, online LDA will work
+  //better when there are more examples, and the expectation maximization optimizer will work
+  //better when there is a larger input vocabulary. This method is also capable of scaling to hundreds
+  //or thousands of topics.
+  //To input our text data into LDA, we’re going to have to convert it into a numeric format. You
+  //can use the CountVectorizer to achieve this.
+
+  import org.apache.spark.ml.feature.{Tokenizer, CountVectorizer}
+  val tkn = new Tokenizer().setInputCol("Description").setOutputCol("DescOut") //so you do not have to write your own split
+  val tokenized = tkn.transform(sales.drop("features"))
+  val cv = new CountVectorizer()
+    .setInputCol("DescOut")
+    .setOutputCol("features")
+    .setVocabSize(500)
+    .setMinTF(0)
+    .setMinDF(0)
+    .setBinary(true)
+  val cvFitted = cv.fit(tokenized)
+  val prepped = cvFitted.transform(tokenized)
+
+  prepped.printSchema()
+  prepped.show(20,false)
+
+  // in Scala
+  import org.apache.spark.ml.clustering.LDA
+  val lda = new LDA().setK(10).setMaxIter(5)
+  println(lda.explainParams())
+  val model = lda.fit(prepped) //all the modelling work is done here
+
+  // in Scala
+  val top3df = model.describeTopics(3)
+  top3df.show(false)
+  println(cvFitted.vocabulary.size)
+
+  //we can get topic words by hand
+  print(cvFitted.vocabulary(0),cvFitted.vocabulary(1),cvFitted.vocabulary(12))
+
+  //TODO make a column in top3df with termIndiced decoded from cvFitted.vocabulary
+  //one way would be to make a udf and then create a column
+
+
 
 
 
